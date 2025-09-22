83ff959eac940ceee59c0ab0a1a038487d2316f4
Yu Jie Teo <yujie@protonmail.com>
2025-09-23 01:59:09 +0800

    feat: add markov chain code

---
+++ markov/markov.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+p_init = np.array([1/3., 1/3., 1/3.]);
+
+assert p_init.sum() == 1;
+
+p_transition = np.array(
+    [[0.90, 0.05, 0.05],
+     [0.01, 0.85, 0.14],
+     [0.06, 0.04, 0.9]]
+)
+
+p_state_t = [p_init];
+
+for i in range(3):
+    assert p_transition[i, :].sum() == 1;
+
+# ergodicity of range? how to know order of magnitude for stationary distribution?
+
+for i in range(100):
+    p_state_t.append(p_state_t[-1] @ p_transition);
+
+state_dist = pd.DataFrame(p_state_t);
+
+# idempotency of transition matrix?
+
+print(state_dist);
